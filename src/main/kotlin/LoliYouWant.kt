b65d7acad860aa835c03bd5655e6620c3227f7fa
ヒューマンワーキング <coolxiaom95@gmail.com>
2022-09-01 13:41:25 +0800

    添加好友消息支持

--- src/main/kotlin/LoliYouWant.kt
+++ src/main/kotlin/LoliYouWant.kt
@@ -9,6 +9,7 @@ import net.mamoe.mirai.console.plugin.id
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.contact.Contact
+import net.mamoe.mirai.event.events.FriendMessageEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.globalEventChannel
 import net.mamoe.mirai.message.MessageReceipt
@@ -33,6 +34,7 @@ object LoliYouWant : KotlinPlugin(
     private val PERM_BYPASS_COOLDOWN = PermissionId(id, "bypass.cooldown")
     private val PERM_RELOAD = PermissionId(id, "reload")
     private val cooldown = mutableMapOf<Long, Long>()
+    private val cooldownFriend = mutableMapOf<Long, Long>()
     override fun onEnable() {
         val permRandom = PermissionService.INSTANCE.register(PERM_RANDOM, "随机发图权限")
         val permBypassCooldown = PermissionService.INSTANCE.register(PERM_BYPASS_COOLDOWN, "绕过冷却时间")
@@ -85,6 +87,53 @@ object LoliYouWant : KotlinPlugin(
                 }
             ))
             group.sendMessage(LoliConfig.replySuccess.replace(replacement))
+            if (LoliConfig.recallFetchingMessage) receipt.recallIgnoreError()
+        }
+
+        globalEventChannel(coroutineContext).subscribeAlways<FriendMessageEvent> {
+            if (!permRandom.testPermission(sender.permitteeId)) return@subscribeAlways
+            if (!LoliConfig.keywords.contains(message.filterIsInstance<PlainText>().joinToString { it.content }.trimStart().trimEnd())) return@subscribeAlways
+            val replacement = mutableMapOf("quote" to QuoteReply(source), "at" to At(sender))
+            if (!permBypassCooldown.testPermission(sender.permitteeId)) {
+                val cd = cooldownFriend.getOrDefault(sender.id, 0)
+                if (cd >= System.currentTimeMillis()) {
+                    replacement["cd"] = PlainText(((cd - System.currentTimeMillis()) / 1000L).toString())
+                    sender.sendMessage(LoliConfig.replyCooldown.replace(replacement))
+                    return@subscribeAlways
+                }
+            }
+            cooldownFriend[sender.id] = System.currentTimeMillis() + LoliConfig.cooldown * 1000
+            val receipt = sender.sendMessage(LoliConfig.replyFetching.replace(replacement))
+            val loli = searchLolis(Lolibooru.get(10, 1, "order%3Arandom%20-rating:e")).randomOrNull()
+            if (loli == null) {
+                sender.sendMessage(LoliConfig.replyFail.replace(replacement))
+                cooldownFriend[sender.id] = System.currentTimeMillis() + LoliConfig.failCooldown * 1000
+                receipt.recallIgnoreError()
+                return@subscribeAlways
+            }
+            val url = when(LoliConfig.quality)
+            {
+                "FILE" -> loli.url
+                "PREVIEW" -> loli.urlPreview
+                else -> loli.urlSample
+            }.replace(" ", "%20")
+            replacement.putAll(mapOf(
+                "id" to PlainText(loli.id.toString()),
+                "previewUrl" to PlainText(loli.urlPreview.replace(" ", "%20")),
+                "sampleUrl" to PlainText(loli.urlSample.replace(" ", "%20")),
+                "fileUrl" to PlainText(loli.url.replace(" ", "%20")),
+                "url" to PlainText(url.replace(" ", "%20")),
+                "tags" to PlainText(loli.tags),
+                "rating" to PlainText(loli.rating),
+                "pic" to PrepareUploadImage.url(sender, url, LoliConfig.imageFailDownload
+                ) { input ->
+                    if (!LoliConfig.download) return@url input
+                    val file = resolveDataFile(url.substringAfterLast('/').replace("%20", " "))
+                    file.writeBytes(input.readBytes())
+                    return@url FileInputStream(file)
+                }
+            ))
+            sender.sendMessage(LoliConfig.replySuccess.replace(replacement))
             if (LoliConfig.recallFetchingMessage) receipt.recallIgnoreError()
         }
         logger.info { "Plugin loaded" }
