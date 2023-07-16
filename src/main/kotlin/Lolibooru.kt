af976569bcdb375102fb86fdd6b783745620eef9
MrXiaoM <coolxiaom95@gmail.com>
2023-07-16 22:05:46 +0800

    update UserAgent

--- src/main/kotlin/Lolibooru.kt
+++ src/main/kotlin/Lolibooru.kt
@@ -75,7 +75,7 @@ object Lolibooru {
         conn.connectTimeout = timeout * 1000
         conn.readTimeout = timeout * 1000
         conn.requestMethod = "GET"
-        conn.addRequestProperty("User-Agent", "Chrome/104.0.5112.81")
+        conn.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.79")
         conn.connect()
 
         return conn.inputStream
